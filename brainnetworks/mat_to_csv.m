f9d9383c7a333dda32dc1f99f07622869614d27a
amurwin <andrew.murwin@gmail.com>
2022-10-18 12:27:00 -0700

    Updated CSVs (1/2)

--- brainnetworks/mat_to_csv.m
+++ brainnetworks/mat_to_csv.m
@@ -2,14 +2,14 @@ directory = dir('smallgraphs')
 
 for file = 3:size(directory)
     filename = directory(file).name
-    load("smallgraphs/" + "M87102217_fiber.mat")
+    load("smallgraphs/" + filename)
     file = fopen("CSVdata\" + filename(1:end-4) + ".csv", "w");
     A = full(fibergraph);
     for row = 1:70
-        for col = 1:69
+        for col = 1:70
             fprintf(file, "%.0f,",A(row, col));
         end
-        fprintf(file, "%.0f\n",A(row, 70));
+        fprintf(file, "\n");
     end
     fclose(file);
 end
