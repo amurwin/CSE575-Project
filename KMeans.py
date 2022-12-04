be2ba7e8a19f3f57e7afeab59a849f136ac9544e
dwdeng <derek.deng64@gmail.com>
2022-12-03 17:55:25 -0700

    revamped KMEANS

--- KMeans.py
+++ KMeans.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import cluster
+from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
@@ -22,27 +23,42 @@ for file in os.listdir():
 sex = getLabel(0, order)
 math = getLabel(1, order)
 creativity = getLabel(2, order)
-
-X_train, X_test, y_train, y_test = train_test_split(data, math, test_size=0.2, random_state=0)
+#Set random seed for reproducibility
+np.random.seed(1000)
 
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X = np.array(data)
 
 n_components = 20
 
 # Built in PCA for quick kmeans
-pcaKMeans = PCA(n_components=n_components, svd_solver='full').fit(X_train)
-X_train_pcaKmeans = pcaKMeans.transform(X_train)
-X_test_pcaKmeans = pcaKMeans.transform(X_test)
+pcaKMeans = PCA(n_components=n_components, svd_solver='full')
 
+X_pca = pcaKMeans.fit_transform(X)
+PCA_components = pd.DataFrame(X_pca)
+ks = range(1, 10)
+inertias = []
+for k in ks:
+    # Create a KMeans.py instance with k clusters: model
+    model = KMeans(n_clusters=k)
+
+    # Fit model to samples
+    model.fit(PCA_components.iloc[:, :3])
+
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+
+plt.plot(ks, inertias)
+plt.xlabel('Number of Clusters, k')
+plt.ylabel('Inertia, k')
+plt.show()
 # KMEANS
-k_means = cluster.KMeans(n_clusters= 5)
-k_means.fit(X_train_pcaKmeans)
-kmeansLabel = k_means.fit_predict(X_test_pcaKmeans)
+k_means = cluster.KMeans(n_clusters= 6)
+k_means.fit(X_pca)
+kmeansLabel = k_means.fit_predict(X_pca)
 unique_labels = np.unique(kmeansLabel)
 for i in unique_labels:
-    plt.scatter(X_test_pcaKmeans[kmeansLabel == i , 0] , X_test_pcaKmeans[kmeansLabel == i , 1] , label = i)
+    plt.scatter(X_pca[kmeansLabel == i , 0] , X_pca[kmeansLabel == i , 1] , label = i)
 plt.title("K-Means Clustering with 5 clusters and PCA")
 plt.legend()
 plt.show()
